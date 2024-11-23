cf7d7bbcf93baa189cc37ab591066ae75a552348
Willington Juma <willingtonejumamconyango@gmail.com>
2024-11-23 23:35:35 +0300

    update

--- Cylinder.java
+++ Cylinder.java
@@ -3,3 +3,12 @@ and length of a cylinder and computes the area and volume using the following
 formulas:
 area = radius * radius * p
 volume = area * lengt */
+class Cylinder {
+    public static void main(String[] args) {
+        int radius=7;
+        int length=7;
+        double volume =radius*radius;
+        double area= radius*length;
+        System.out.println("The area is "+volume +"and volume is "+area);
+    }
+}
